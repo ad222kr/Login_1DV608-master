43a8eece3a18414123ff4dda12a6783a2d5319e7
ad222kr <ad222kr@student.lnu.se>
2015-10-09 00:13:10 +0200

    Indentation with spaces

--- src/view/RegisterView.php
+++ src/view/RegisterView.php
@@ -13,119 +13,119 @@ namespace view;
 
 class RegisterView extends BaseView {
 
-	private static $registerName = "?register";
-	private static $messageID = "RegisterView::Message";
-	private static $usernameID = "RegisterView::UserName";
-	private static $passwordID = "RegisterView::Password";
-	private static $passwordRepeatID = "RegisterView::PasswordRepeat";
-	private static $register = "RegisterView::Register";
+    private static $registerName = "?register";
+    private static $messageID = "RegisterView::Message";
+    private static $usernameID = "RegisterView::UserName";
+    private static $passwordID = "RegisterView::Password";
+    private static $passwordRepeatID = "RegisterView::PasswordRepeat";
+    private static $register = "RegisterView::Register";
 
-	private static $credentialsMissingMessage = "Username has too few characters, at least 3 characters. Password has too few characters, at least 6 characters.";
-	private static $passwordToShortOrMissingMessage = "Password has too few characters, at least 6 characters.";
-	private static $usernameToShortMessage = "Username has too few characters, at least 3 characters.";
-	private static $passwordDoNotMatchMessage = "Passwords do not match.";
-	private static $registrationSuccessMessage = "Registered new user.";
-	private static $userAlreadyExistsMessage = "User exists, pick another username.";
-	private static $notAllowedCharactersInUsernameMessage = "Username contains invalid characters.";
-	private static $databaseErrorMessage = "Something went wrong when saving data";
+    private static $credentialsMissingMessage = "Username has too few characters, at least 3 characters. Password has too few characters, at least 6 characters.";
+    private static $passwordToShortOrMissingMessage = "Password has too few characters, at least 6 characters.";
+    private static $usernameToShortMessage = "Username has too few characters, at least 3 characters.";
+    private static $passwordDoNotMatchMessage = "Passwords do not match.";
+    private static $registrationSuccessMessage = "Registered new user.";
+    private static $userAlreadyExistsMessage = "User exists, pick another username.";
+    private static $notAllowedCharactersInUsernameMessage = "Username contains invalid characters.";
+    private static $databaseErrorMessage = "Something went wrong when saving data";
 
-	private $registerModel;
+    private $registerModel;
 
-	public function __construct(\common\ITempMessageHandler $tempMessageHandler, \model\RegisterModel $registerModel) {
-		parent::__construct($tempMessageHandler);
-		$this->registerModel = $registerModel;
-	}
+    public function __construct(\common\ITempMessageHandler $tempMessageHandler, \model\RegisterModel $registerModel) {
+        parent::__construct($tempMessageHandler);
+        $this->registerModel = $registerModel;
+    }
 
-	public function response() {
-		$message = $this->getMessage();
-		return $this->generateForm($message);
-	}
+    public function response() {
+        $message = $this->getMessage();
+        return $this->generateForm($message);
+    }
 
-	public function setRegistrationSuccess() {
-		$this->setMessage("Registered new user.", true);
-		$_SESSION["username"] = $this->getRequestUsername(); //TODO: remove str-dep
-		header("Location: ?");
-		exit();
-	}
+    public function setRegistrationSuccess() {
+        $this->setMessage("Registered new user.", true);
+        $_SESSION["username"] = $this->getRequestUsername(); //TODO: remove str-dep
+        header("Location: ?");
+        exit();
+    }
 
-	public function getRegistrationCredentials() {
+    public function getRegistrationCredentials() {
 
-		try {
-			$username = $this->getRequestUsername();
-			$password = $this->getRequestPassword();
-			$repeatPassword = $this->getRequestRepeatPassword();
+        try {
+            $username = $this->getRequestUsername();
+            $password = $this->getRequestPassword();
+            $repeatPassword = $this->getRequestRepeatPassword();
 
-			return new \model\RegisterCredentials($username, $password, $repeatPassword);
+            return new \model\RegisterCredentials($username, $password, $repeatPassword);
 
-		} catch (\common\RegistrationCredentialsMissingException $e) {
-			$this->setMessage(self::$credentialsMissingMessage, true);
-		} catch(\common\UsernameToShortException $e) {
-			$this->setMessage(self::$usernameToShortMessage, true);
-		} catch(\common\NotAllowedCharactersInUsernameException $e) {
-			$this->setMessage(self::$notAllowedCharactersInUsernameMessage, true);
-		} catch(\common\PasswordToShortException $e) {
-			$this->setMessage(self::$passwordToShortOrMissingMessage, true);
-		} catch(\common\PasswordDoNotMatchException $e){
-			$this->setMessage(self::$passwordDoNotMatchMessage, true);
-		}
-	}
+        } catch (\common\RegistrationCredentialsMissingException $e) {
+            $this->setMessage(self::$credentialsMissingMessage, true);
+        } catch(\common\UsernameToShortException $e) {
+            $this->setMessage(self::$usernameToShortMessage, true);
+        } catch(\common\NotAllowedCharactersInUsernameException $e) {
+            $this->setMessage(self::$notAllowedCharactersInUsernameMessage, true);
+        } catch(\common\PasswordToShortException $e) {
+            $this->setMessage(self::$passwordToShortOrMissingMessage, true);
+        } catch(\common\PasswordDoNotMatchException $e){
+            $this->setMessage(self::$passwordDoNotMatchMessage, true);
+        }
+    }
 
-	public function setDatabaseError() {
-		$this->setMessage(sefl::$databaseErrorMessage, true);
-	}
+    public function setDatabaseError() {
+        $this->setMessage(sefl::$databaseErrorMessage, true);
+    }
 
-	public function setRegistrationFailed() {
-		$this->setMessage(self::$userAlreadyExistsMessage, true);
-	}
+    public function setRegistrationFailed() {
+        $this->setMessage(self::$userAlreadyExistsMessage, true);
+    }
 
-	public function setRegistrationSuccessful() {
-		$this->setMessage(self::$registrationSuccessMessage, true);
+    public function setRegistrationSuccessful() {
+        $this->setMessage(self::$registrationSuccessMessage, true);
 
-	}
+    }
 
-	private function getRequestUsername() {
-		if (isset($_POST[self::$usernameID])) {
-			return $_POST[self::$usernameID];
-		}
-		return "";
-	}
+    private function getRequestUsername() {
+        if (isset($_POST[self::$usernameID])) {
+            return $_POST[self::$usernameID];
+        }
+        return "";
+    }
 
-	private function getRequestPassword(){
-		if (isset($_POST[self::$passwordID])) {
-			return $_POST[self::$passwordID];
-		}
-		return "";
-	}
+    private function getRequestPassword(){
+        if (isset($_POST[self::$passwordID])) {
+            return $_POST[self::$passwordID];
+        }
+        return "";
+    }
 
-	private function getRequestRepeatPassword() {
-		if (isset($_POST[self::$passwordRepeatID])) {
-			return $_POST[self::$passwordRepeatID];
-		}
-	}
+    private function getRequestRepeatPassword() {
+        if (isset($_POST[self::$passwordRepeatID])) {
+            return $_POST[self::$passwordRepeatID];
+        }
+    }
 
-	private function generateForm($message) {
+    private function generateForm($message) {
 
-		return '<form action="' . self::$registerName . '" method="post" enctype="multipart/formdata">
-				<fieldset>
-					<legend>Register a new user - Write username and password</legend>
-					<p id="' . self::$messageID . '">' . $message . '</p>
-					<label for="' . self::$usernameID . '" >Username : </label>
-					<input type="text" size="20" name="' . self::$usernameID . '" id="' . self::$usernameID . '" value="'. strip_tags($this->getRequestUsername()) .'" />
-					<br/>
-					<label for="' . self::$passwordID . '" >Password :</label>
-					<input type="password" size="20" name="' . self::$passwordID . '" id="' . self::$passwordID . '" value="" />
-					<br/>
-					<label for="' . self::$passwordRepeatID . '" >Repeat password :</label>
-					<input type="password" size="20" name="' . self::$passwordRepeatID . '" id="' . self::$passwordRepeatID . '" value="" />
-					<br/>
-					<input id="submit" type="submit" name="' . self::$register .'" value="Register" />
-					<br/>
-				</fieldset>
-				</form>';
-	}
+        return '<form action="' . self::$registerName . '" method="post" enctype="multipart/formdata">
+                <fieldset>
+                    <legend>Register a new user - Write username and password</legend>
+                    <p id="' . self::$messageID . '">' . $message . '</p>
+                    <label for="' . self::$usernameID . '" >Username : </label>
+                    <input type="text" size="20" name="' . self::$usernameID . '" id="' . self::$usernameID . '" value="'. strip_tags($this->getRequestUsername()) .'" />
+                    <br/>
+                    <label for="' . self::$passwordID . '" >Password :</label>
+                    <input type="password" size="20" name="' . self::$passwordID . '" id="' . self::$passwordID . '" value="" />
+                    <br/>
+                    <label for="' . self::$passwordRepeatID . '" >Repeat password :</label>
+                    <input type="password" size="20" name="' . self::$passwordRepeatID . '" id="' . self::$passwordRepeatID . '" value="" />
+                    <br/>
+                    <input id="submit" type="submit" name="' . self::$register .'" value="Register" />
+                    <br/>
+                </fieldset>
+                </form>';
+    }
 
-	public function userPressedRegister() {
-		return isset($_POST[self::$register]);
-	}
+    public function userPressedRegister() {
+        return isset($_POST[self::$register]);
+    }
 }
 
